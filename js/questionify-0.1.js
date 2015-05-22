1083d0860c1e4f320280533601b168eefb47e64e
Pete Davis <php.wizardry@gmail.com>
2015-05-22 22:13:01 +0100

    added debugData declaration, hideafter declaration, debug if() statements, stooped every question looping every click when checking for rollbacks,

--- js/questionify-0.1.js
+++ js/questionify-0.1.js
@@ -1,10 +1,15 @@
 var questionify = {
 	questionadata : {},
 	debug : false,
+	debugData : [],
 	container : 'container',
+	hideafter : false,
 
 	setQuestionData : function(q) {
-		this.questiondata = q;
+		questionify.questiondata = q;
+		if(questionify.debug) {
+			console.log("QuestionData Added");
+		}
 	},
 	showQuestionHtml : function() {
 		$.each(this.questiondata, function(q, data) {
@@ -21,39 +26,60 @@ var questionify = {
 
 			//finish the HTML
 			htmlstring = htmlstring + '</ul></div>'
-			$(parent.container).append(htmlstring);
+			$(parent.container).append(htmlstring);	
 		});
+
+		
+		if(questionify.debug) {
+			console.log("QuestionHTML Appended");
+		}
 
 		$(parent.container).find("#q1").show(200);
 	},
 	questionClick : function(q) {
-		
+		if(questionify.debug) {
+			console.log("Question Clicked: " + q.data('id')); ///todo breakdown obj
+		}
 
 		var next = q.data('next');
-			var current = q.data('id');
-			
+		var current = q.data('id');
+		
 
-			$.each(this.questiondata, function(question, data) {
-				var qid = question.substr(1);
-				var qnext = next.substr(1);
-				var qcurrent = current.substr(1);
+		$.each(questionify.questiondata, function(question, data) {
+			var qid = question.substr(1);
+			var qnext = next.substr(1);
+			var qcurrent = current.substr(1);
 
-				if(parseInt(qid) > parseInt(qcurrent)) {
-					$(parent.container).find("#q" + qid).hide(200);
+			if(parseInt(qid) > parseInt(qcurrent) && $(parent.container).find("#q" + qid).prop('visible')) {
+				$(parent.container).find("#q" + qid).hide(200);
+
+				if(questionify.debug) {
+					console.log("Question Hidden: " + qid); ///todo breakdown obj
 				}
+			}
 
-			});
+		});
 
-			$('#' + next).show(200);
+		if(questionify.hideafter == true) {
+			$('#' + current).hide(200);
+		}
+		
+		$('#' + next).show(200);
+
+
 	},
 	setContainer : function(c) {
-		this.container = c;
+		questionify.container = c;
+		if(questionify.debug) {
+			console.log("Container Set: " + c); ///todo breakdown obj
+		}
 	},
-	flowify : function(q, container, debug) {
+	flowify : function(q, container, debug, hideafter) {
 		
-		this.setQuestionData(q);
-		this.container = container || this.container;
-		this.debug = debug || this.debug;
+		questionify.debug = debug || questionify.debug;
+		questionify.hideafter = hideafter || questionify.hideafter;
+		questionify.setContainer(container || questionify.container);
+		questionify.setQuestionData(q);
 		this.showQuestionHtml();
 	},
 	getQuestions : function() {
